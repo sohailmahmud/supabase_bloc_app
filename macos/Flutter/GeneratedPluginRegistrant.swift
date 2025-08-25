5b4a12657bea45a8404736cd57f6dc6e9c519470
Sohail Mahmud <sohailmahmud@yahoo.com>
2025-08-25 14:42:02 +0600

    Update Flutter SDK version to 3.35.1 and adjust dependencies in pubspec.yaml for improved compatibility and functionality, including updates to Firebase, Supabase, and local notifications. Also, register new plugins in macOS and Windows for enhanced features.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,15 +8,21 @@ import Foundation
 import app_links
 import firebase_core
 import firebase_messaging
+import flutter_local_notifications
 import path_provider_foundation
 import shared_preferences_foundation
+import sign_in_with_apple
 import url_launcher_macos
+import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
